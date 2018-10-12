d13605d7c4b4eb0d82ac47ff360279edb9555b26
Shaun Dawson <shaundawson0883@gmail.com>
2018-10-11 23:02:33 -0400

    initial commit

---
+++ contactmanager/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Contact from './components/Contact';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1> The App Component </h1>
+      <Contact />
+      </div>
+    );
+  }
+}
+
+export default App;
