1cdfa0f3b36bc0ed1c69ee7912a5cdcc19c4a75f
Shaun Dawson <shaundawson0883@gmail.com>
2018-10-12 00:32:36 -0400

    implement bootstrap

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header />
-      <Contact name = "Shaun Dawson"
-      email = "shaund@gmail.com" phone="347-300-1243"
-      />
-      <Contact
-      name = "Nandi Alman"
-      email= "nandi@gmail.com"
-      phone = "123-456-4762"
-      />
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact
+            name="Shaun Dawson"
+            email="shaund@gmail.com"
+            phone="347-300-1243"
+          />
+          <Contact
+            name="Nandi Alman"
+            email="nandi@gmail.com"
+            phone="123-456-4762"
+          />
+        </div>
       </div>
     );
   }
--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -6,19 +6,19 @@ class Contact extends Component {
   render() {
     const { name, email, phone } = this.props;
     return (
-      <div>
+      <div className="card card-body mb-3">
         <h4> { name }</h4>
-          <ul>
-            <li>{ email }</li>
-            <li>{ phone }</li>
-          </ul>
+        <ul className="list-group">
+          <li className="list-group-item">{ email }</li>
+          <li className="list-group-item">{ phone }</li>
+        </ul>
       </div>
     );
   }
 }
 
 Contact.propTypes = {
-  name: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
   phone: PropTypes.string.isRequired,
 };
