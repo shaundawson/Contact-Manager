3ccf7df1a7b520f9fa0aa29cba66d40c11fd2707
Shaun Dawson <shaundawson0883@gmail.com>
2018-10-12 01:32:21 -0400

    added icon and click event

--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -3,11 +3,19 @@ import PropTypes from 'prop-types';
 
 
 class Contact extends Component {
+  state = {};
+
+  onShowClick = e => {
+    console.log(e);
+  };
+
   render() {
     const { contact } = this.props;
     return (
       <div className="card card-body mb-3">
-        <h4> { contact.name }</h4>
+        <h4> { contact.name } <i onClick={this.onShowClick}
+        className="fas fa-sort-down" />
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">{ contact.email }</li>
           <li className="list-group-item">{ contact.phone }</li>
